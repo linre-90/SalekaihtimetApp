df66b1aeda40683f1e6eafb924c343bcd73d57c6
linre-90 <juho.lindemark@gmail.com>
2024-03-12 16:52:50 +0200

    Locating user now works.

--- app/src/main/java/com/example/salekaihtimetapp/LocationSelector.kt
+++ app/src/main/java/com/example/salekaihtimetapp/LocationSelector.kt
@@ -3,25 +3,29 @@ package com.example.salekaihtimetapp
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
+/**
+ * Location query button. Handles permissions.
+ * */
 fun LocationSelector(setLatitude: (Float) -> Unit, setLongitude: (Float) -> Unit){
     val locationPermission = rememberMultiplePermissionsState(
         listOf(
@@ -29,44 +33,62 @@ fun LocationSelector(setLatitude: (Float) -> Unit, setLongitude: (Float) -> Unit
             Manifest.permission.ACCESS_FINE_LOCATION
         )
     )
-    val showRationalDialog = remember { mutableStateOf(false) }
-
+    var findingLocation by remember { mutableStateOf(false) }
+    var locationMsg by remember { mutableStateOf("") }
     val context = LocalContext.current
+
+    // Button to start user locating
     Button(
+        enabled = !findingLocation,
         modifier =  Modifier.padding(0.dp, 20.dp),
         onClick = {
             if (!locationPermission.allPermissionsGranted) {
-                if (locationPermission.shouldShowRationale) {
-                    // Show a rationale if needed (optional)
-                    showRationalDialog.value = true
-                } else {
-                    // Request the permission
-                    locationPermission.launchMultiplePermissionRequest()
-                }
+                locationPermission.launchMultiplePermissionRequest()
             } else {
-                Log.d("PERMISSION_LOC", "Already granted")
-                getLocation(context)
+                queryLocation(
+                    context =  context,
+                    onFindingLocation = { newVal -> findingLocation = newVal },
+                    setLocatingStatus = {newVal -> locationMsg = newVal},
+                    updateLatitude = {lat -> setLatitude(lat)},
+                    updateLongitude = {lat -> setLongitude(lat)},
+                )
             }
     }) {
-        Text("Find location")
+        if(locationMsg.isNotEmpty() && !findingLocation){
+            Text(text=locationMsg)
+        }else if(findingLocation){
+            Text("...Getting location...")
+        } else{
+            Text("Find location")
+        }
     }
 }
 
+/**
+ * Find users current latitude and longitude.
+ * */
 @SuppressLint("MissingPermission")
-fun getLocation(context: Context){
+fun queryLocation(
+    context: Context,
+    onFindingLocation: (Boolean) -> Unit,
+    setLocatingStatus: (String) -> Unit,
+    updateLongitude: (Float) -> Unit,
+    updateLatitude: (Float) ->Unit
+){
+    onFindingLocation(true)
     val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
-    fusedLocationClient.lastLocation.addOnSuccessListener {
-        Log.d("PERMISSION_LOC", (it == null).toString())
-    }.addOnCanceledListener {
-
-    }
-
-
-
-//    fusedLocationClient.lastLocation.addOnSuccessListener {
-//        location -> Log.d("PERMISSION_LOC", (location == null).toString())
-//    }
-
+    fusedLocationClient.getCurrentLocation( Priority.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token)
+        .addOnSuccessListener {
+            if(it != null){
+                setLocatingStatus("Location found, click to retry.")
+                updateLongitude(it.longitude.toFloat())
+                updateLatitude(it.latitude.toFloat())
+                onFindingLocation(false)
+            }
+        }.addOnCanceledListener {
+            setLocatingStatus("Location search cancelled.")
+            onFindingLocation(false)
+        }
 }
 
--- app/src/main/java/com/example/salekaihtimetapp/SettingsForm.kt
+++ app/src/main/java/com/example/salekaihtimetapp/SettingsForm.kt
@@ -1,5 +1,6 @@
 package com.example.salekaihtimetapp
 
+import android.app.Application
 import android.icu.util.TimeZone
 import android.util.Log
 import androidx.compose.foundation.layout.Column
@@ -29,19 +30,27 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import android.os.Build
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import kotlin.system.exitProcess
 
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
+/**
+ * Render user settings form.
+ * */
 fun SettingsForm(){
     // Create states
     val timePickerState by remember { mutableStateOf(TimePickerState(8, 0, is24Hour = true)) }
@@ -49,6 +58,7 @@ fun SettingsForm(){
     var durationState by remember{ mutableStateOf("8") }
     var latitudeState by remember { mutableFloatStateOf(63.096F) }
     var longitudeState by remember { mutableFloatStateOf(21.61577F) }
+    var context = LocalContext.current
 
     // Render form if not loading
     if(!loading){
@@ -56,15 +66,18 @@ fun SettingsForm(){
             Modifier
                 .padding(20.dp)
                 .verticalScroll(rememberScrollState())){
+            // User locating
             FormText(name = "Find your location", hint = "Use phone gps to get location.")
             LocationSelector(
                 setLatitude = {value -> latitudeState = value},
                 setLongitude = {value -> longitudeState = value}
             )
             Divider()
+            // Force closing time hour:min input
             FormText(name = "Force close time", hint = "Forces curtain to close")
             TimePicker(state = timePickerState, Modifier.padding(0.dp, 20.dp))
             Divider()
+            // Closing time duration
             FormText(name = "Close time duration", hint = "Force close duration." )
             TextField(
                 modifier = Modifier.padding(0.dp, 20.dp),
@@ -72,6 +85,7 @@ fun SettingsForm(){
                 onValueChange = { newVal -> durationState = newVal},
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
             Divider()
+            // Upload button
             FormText(name = "Upload settings to curtains.", hint = "Before submitting make sure you are connected via bluetooth.")
             Button(
                 modifier = Modifier.padding(0.dp, 20.dp),
@@ -85,8 +99,26 @@ fun SettingsForm(){
                         durationState
                     )
 
-                    handleSettingsSubmit({ isLoading -> loading = isLoading }, userSettings)
+                    val ok = handleSettingsSubmit({ isLoading -> loading = isLoading }, userSettings)
+
+                    if(ok){
+                        Toast.makeText(context, "Settings uploaded to device successfully.", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(context, "Failed to upload settings.", Toast.LENGTH_SHORT).show()
+                    }
+                    Thread.sleep(500)
+                    exitProcess(0)
             }) { Text(text = "Upload") }
+            // Cancel upload connection
+            Button(
+                modifier = Modifier.padding(0.dp, 20.dp),
+                colors = ButtonDefaults.buttonColors(Color.Red),
+                onClick = {
+                    handleCancelUpload()
+                    Toast.makeText(context, "No new settings uploaded to device.", Toast.LENGTH_SHORT).show()
+                    Thread.sleep(500)
+                    exitProcess(0)
+                }) { Text(text = "Cancel") }
         }
     }else{
         // Loading indicator
@@ -115,7 +147,12 @@ private fun Loading(){
     }
 }
 
-private fun handleSettingsSubmit(onSetLoading: (Boolean) -> Unit, userSettings: UserSettings){
+/**
+ * Upload user settings to device.
+ * Sends new settings with post request.
+ * Device is set back to operation mode, closing upload mode and web server.
+ * */
+private fun handleSettingsSubmit(onSetLoading: (Boolean) -> Unit, userSettings: UserSettings): Boolean{
     onSetLoading(true)
     // year, month, day does not matter... These are in local time
     val openLDT = LocalDateTime.of(1980, 7, 10, userSettings.closeTimeHour.toInt(), userSettings.closeTimeMinute.toInt(), 0)
@@ -128,4 +165,13 @@ private fun handleSettingsSubmit(onSetLoading: (Boolean) -> Unit, userSettings: 
     val data = "close_start=${closeUTC.hour}:${closeUTC.minute};close_duration=${userSettings.closeDuration};latitude=${userSettings.latitude};longitude=${userSettings.longitude};"
     Log.d("USER_DATA", data)
     onSetLoading(false)
+    return true
+}
+
+/**
+ * Send simple get request to server.
+ * Get request changes device mode back to operation, exiting from setup mode and closing web server.
+ * */
+private fun handleCancelUpload():Boolean{
+    return true
 }
